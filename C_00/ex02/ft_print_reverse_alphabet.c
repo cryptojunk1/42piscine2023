a65e006d17289ec1a55d93220db947c2906cb3ef
Rene Messner <rmessner@c2r1p7.42vienna.com>
2023-04-24 17:14:51 +0200

    Added my files

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmessner <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 17:13:00 by rmessner          #+#    #+#             */
+/*   Updated: 2023/04/22 17:19:03 by rmessner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+
+void	ft_print_reverse_alphabet(void)
+{
+		char c;
+		c = 'z';
+
+		while(c >= 'a')
+		{
+			write(1, &c, 1);
+			c--;
+		}
+}
+
+/*
+int		main()
+{
+		ft_print_reverse_alphabet();
+		return (0);
+}
+*/
