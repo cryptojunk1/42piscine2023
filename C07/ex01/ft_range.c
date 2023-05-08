7a65be9f469a494c0c3c13db81459a4a313b705f
René Messner <remes@LAPTOP-3QJU7RD3.localdomain>
2023-05-08 21:13:33 +0200

    added c07

---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmessner <rmessner@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 21:09:18 by rmessner          #+#    #+#             */
+/*   Updated: 2023/05/08 21:09:44 by rmessner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int min, int max)
+{
+	int		range;
+	int		index;
+	int		*buffer;
+	int		*d;
+
+	if (min >= max)
+		return (0);
+	range = max - min;
+	d = (buffer = malloc(range * sizeof(int)));
+	if (!d)
+		return (0);
+	index = 0;
+	while (index < range)
+	{
+		buffer[index] = min + index;
+		index++;
+	}
+	return (buffer);
+}
+
+/*
+int		main(void)
+{
+	int	min;
+	int	max;
+	int	*tab;
+	int	i = 0;
+	int	size;
+
+	min = 5;
+	max = 10;
+	size = max - min;
+	tab = ft_range(min, max);
+	while(i < size)
+	{
+		printf("%d, ", tab[i]);
+		i++;
+	}
+
+}
+*/
